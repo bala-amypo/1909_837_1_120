acb70f73841c3d5330ea2f8b51e22fa869b37ab1
bala-amypo <bala@amypo.in>
2025-12-20 16:40:33 +0000

    User commit

--- src/main/java/com/example/demo/ApiKey.java
+++ src/main/java/com/example/demo/ApiKey.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
 @Entity
@@ -11,9 +13,11 @@ public class ApiKey {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="key_value", nullable=false, unique=true)
+    @NotBlank
+    @Column(name = "key_value", nullable = false, unique = true)
     private String keyValue;
 
+    @NotNull
     @Column(nullable = false)
     private Long ownerId;
 
@@ -24,12 +28,84 @@ public class ApiKey {
     @Column(nullable = false)
     private Boolean active = true;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private Timestamp createdAt;
 
     @Column(nullable = false)
     private Timestamp updatedAt;
+    
+    
+    public ApiKey() {
+    }
+    
 
-    // getters & setters
+    public ApiKey(Long id, String keyValue, Long ownerId, QuotaPlan plan, Boolean active, Timestamp createdAt,
+            Timestamp updatedAt) {
+        this.id = id;
+        this.keyValue = keyValue;
+        this.ownerId = ownerId;
+        this.plan = plan;
+        this.active = active;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKeyValue() {
+        return keyValue;
+    }
+
+    public void setKeyValue(String keyValue) {
+        this.keyValue = keyValue;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public QuotaPlan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(QuotaPlan plan) {
+        this.plan = plan;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+      
 }
-
