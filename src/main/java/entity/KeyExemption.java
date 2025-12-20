3c3a7dc8a8f0bd9ad5543f20df102e98e35b9018
bala-amypo <bala@amypo.in>
2025-12-20 07:35:22 +0000

    User commit

---
+++ src/main/java/entity/KeyExemption.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="key_exemption")
+public class KeyExemption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="api_key_id", nullable=false)
+    private ApiKey apiKey;
+
+    private String notes;
+
+    @Column(nullable=false)
+    private Boolean unlimitedAccess = false;
+
+    @Min(0)
+    private Integer temporaryExtensionLimit;
+
+    @Column(nullable=false)
+    private Timestamp validUntil;
+
+    // getters setters
+}
