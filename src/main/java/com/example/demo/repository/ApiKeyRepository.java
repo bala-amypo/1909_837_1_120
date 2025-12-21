59ac607452c09e07432fde1bd084bbe0b50976ff
bala-amypo <bala@amypo.in>
2025-12-21 19:36:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApiKeyRepository.java
+++ src/main/java/com/example/demo/repository/ApiKeyRepository.java
@@ -1,10 +1,19 @@
 package com.example.demo.repository;
 
+
+
 import com.example.demo.entity.ApiKey; // Corrected package from .model to .entity
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
+
+
 
 public interface ApiKeyRepository extends JpaRepository<ApiKey, Long> {
 
+
+
     Optional<ApiKey> findByKeyValue(String keyValue);
+
 }
