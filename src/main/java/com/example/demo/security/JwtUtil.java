cb8c8c984428c9425678a08ba4db6f5f277f59b0
bala-amypo <bala@amypo.in>
2025-12-27 12:41:58 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -2,14 +2,17 @@ package com.example.demo.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.Map;
 
+@Component
 public class JwtUtil {
 
     private static final long EXP = 86400000L;
+
     private static final String SECRET =
             "9a4f2c8d3b7a1e6f45c8a0b3f267d8b1d4e6f3c8a9d2b5f8e3a9c8b5f6d7a4b2";
 
@@ -40,7 +43,7 @@ public class JwtUtil {
     }
 
     public boolean isTokenValid(String token, String username) {
-        return getUsername(token).equals(username);
+        return username.equals(getUsername(token));
     }
 
     public long getExpirationMillis() {
